42e7114c5a26045268fa82c2f3e72a9a113c146c
Gustavo Nascimento <gustavonascimento.gn@gmail.com>
2019-12-11 22:48:39 +0000

    Inclusao das bibliotecas referente a autenticação"

--- gobbiconsultoria/urls.py
+++ gobbiconsultoria/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('', include('apps.core.urls')),
     path('empregados/', include('apps.empregados.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
