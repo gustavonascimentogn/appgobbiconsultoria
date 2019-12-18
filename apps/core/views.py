8250e1ab6ff683b540fbfe32caa3dbba509b8e62
Gustavo Nascimento <gustavonascimento.gn@gmail.com>
2019-12-18 12:24:39 +0000

    Inclusão do nome do funcionário na página de login

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from apps.empregados.models import Empregado
 
 @login_required
 def home(request):
-    return render(request, 'core/index.html')
+    data = {} #dicionario
+    data['usuario'] = request.user
+    return render(request, 'core/index.html', data)
--- apps/empregados/models.py
+++ apps/empregados/models.py
@@ -4,8 +4,8 @@ from apps.empresas.models import Empresa
 
 class Empregado(models.Model):
     nome = models.CharField(max_length=100, blank=False, help_text='Nome completo da pessoa ou funcionário que terá acesso ao sistema')
-    user = models.ForeignKey(User, on_delete=models.PROTECT, default=None)
-    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, default=None)
+    user = models.OneToOneField(User, on_delete=models.PROTECT, null=False, blank=False)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, null=False, blank=False)
 
     def __str__(self):
         return self.nome
