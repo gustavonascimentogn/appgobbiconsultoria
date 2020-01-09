bb2a973d27ec8ff9bc38db3d8e753d5fb4f71c64
Gustavo Nascimento <gustavonascimento.gn@gmail.com>
2020-01-08 22:46:51 -0300

    inclusão do CRUD TiposServicos. Está aparecendo um erro após salvar as alterações ou gravações, mas o registro é salvo normalmente. Mensagem de erro: quote_from_bytes() expected bytes

---
+++ apps/tiposServicos/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import ClientesList, ClienteEdit, ClienteDelete, ClienteNovo
+
+urlpatterns = [
+    path('', ClientesList.as_view(), name='list_clientes'),
+    path('editar/<int:pk>', ClienteEdit.as_view(), name='update_cliente'),
+    path('delete/<int:pk>', ClienteDelete.as_view(), name='delete_cliente'),
+    path('novo', ClienteNovo.as_view(), name='create_cliente'),
+
+]
