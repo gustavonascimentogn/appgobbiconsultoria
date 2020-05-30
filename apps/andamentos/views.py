e59237c8563a6d191623ccaad09e1d66c8df8edf
Gustavo Nascimento <gustavonascimento.gn@gmail.com>
2020-05-30 12:20:26 -0300

    correção

--- apps/andamentos/views.py
+++ apps/andamentos/views.py
@@ -9,6 +9,11 @@ from django.conf import settings
 import requests
 import json
 
+from ..pedidos.models import Pedido
+from ..servicos.models import Servico
+from ..solicitacoes.models import Solicitacao
+
+
 class AndamentosList(ListView):
     model = Andamento
     paginate_by = 20
@@ -31,15 +36,15 @@ class AndamentoEdit(UpdateView):
             if andamento.pedido:
                 emailContato = andamento.pedido.cliente.emailContato ## para quem vai a mensagem
                 player_id_onesignal = andamento.pedido.cliente.playerId_onesignal
-                message = 'Referente ao Contrato/Serviço ID: ' + str(andamento.pedido.pk) + ' | Status atual: ' + andamento.status.nome + ' | Informação adicionada: ' + andamento.comentario
-                html_message= 'Este e-mail refere-se ao Contrato/Serviço <b>ID: ' + str(andamento.pedido.pk) + '</b><br>Status atual: <b>' + andamento.status.nome + '</b><br>Informação adicionada: <b>' + andamento.comentario + '</b>'
-                message_onesignal = 'Atualização referente ao Contrato/Serviço ID: ' + str(andamento.pedido.pk) + ' disponível no App. Acesse e confira o andamento mais recente.'
+                message = 'Referente ao Contrato/Serviço "' + str(andamento.servico.nome) + '" | Status atual: ' + andamento.status.nome + ' | Informação adicionada: ' + andamento.comentario
+                html_message= 'Este e-mail refere-se ao Contrato/Serviço <b>ID: "' + str(andamento.servico.nome) + '"</b><br>Status atual: <b>' + andamento.status.nome + '</b><br>Informação adicionada: <b>' + andamento.comentario + '</b>'
+                message_onesignal = 'Atualização referente ao Contrato/Serviço "' + str(andamento.servico.nome) + '" disponível no App. Acesse e confira o andamento mais recente.'
             else: #elif (self.kwargs['origem'] is 'solicitacao'):
                 emailContato = andamento.solicitacao.cliente.emailContato ## para quem vai a mensagem
                 player_id_onesignal = andamento.solicitacao.cliente.playerId_onesignal
-                message = 'Referente a Solicitação ' + andamento.solicitacao.solicitacao + ' | Status atual: ' + andamento.status.nome + ' | Informação adicionada: ' + andamento.comentario
-                html_message= 'Este e-mail refere-se a Solicitação <b> ID: ' + andamento.solicitacao.solicitacao + '</b><br>Status atual: <b>' + andamento.status.nome + '</b><br>Informação adicionada: <b>' + andamento.comentario + '</b>'
-                message_onesignal = 'Atualização referente a Solicitação ID: ' + andamento.solicitacao.solicitacao + ' disponível no App. Acesse e confira o andamento mais recente.'
+                message = 'Referente a Solicitação "' + andamento.solicitacao.solicitacao + '" | Status atual: ' + andamento.status.nome + ' | Informação adicionada: ' + andamento.comentario
+                html_message= 'Este e-mail refere-se a Solicitação <b> "' + andamento.solicitacao.solicitacao + '"</b><br>Status atual: <b>' + andamento.status.nome + '</b><br>Informação adicionada: <b>' + andamento.comentario + '</b>'
+                message_onesignal = 'Atualização referente a Solicitação "' + andamento.solicitacao.solicitacao + '" disponível no App. Acesse e confira o andamento mais recente.'
             email_from = self.request.user.empregado.email #settings.EMAIL_HOST_USER
             recipient_list = [emailContato, email_from,]
             send_mail( subject, message, email_from, recipient_list,html_message=html_message,auth_user=settings.EMAIL_HOST_USER, auth_password=settings.EMAIL_HOST_PASSWORD)
@@ -73,6 +78,12 @@ class AndamentoNovo(CreateView):
 
     def form_valid(self, form):
         andamento = form.save(commit=False)
+        origem = self.kwargs['origem']
+        if origem == 'pedido' or origem == 'cliente':
+            andamento.pedido = Pedido.objects.get(pk=self.kwargs['pk'])
+            andamento.servico = Servico.objects.get(pk=self.kwargs['servico'])
+        else:
+            andamento.solicitacao = Solicitacao.objects.get(pk=self.kwargs['pk'])
         andamento.save()
 
         ## ESSE CODIGO SE REPETE NO EDIT E NO CREATE
@@ -80,18 +91,18 @@ class AndamentoNovo(CreateView):
             ## enviando e-mail para o cliente, caso campo esteja marcado como True
             nomeFantasia = self.request.user.empregado.empresa.nomeFantasia ## da empresa que enviara o email
             subject = '['+ nomeFantasia + '] Novidade referente ao seu atendimento'
-            if andamento.pedido:
+            if origem == 'pedido' or origem == 'cliente':
                 emailContato = andamento.pedido.cliente.emailContato ## para quem vai a mensagem
                 player_id_onesignal = andamento.pedido.cliente.playerId_onesignal
-                message = 'Referente ao Contrato/Serviço ID: ' + str(andamento.pedido.pk) + ' | Status atual: ' + andamento.status.nome + ' | Informação adicionada: ' + andamento.comentario
-                html_message= 'Este e-mail refere-se ao Contrato/Serviço <b>ID: ' + str(andamento.pedido.pk) + '</b><br>Status atual: <b>' + andamento.status.nome + '</b><br>Informação adicionada: <b>' + andamento.comentario + '</b>'
-                message_onesignal = 'Atualização referente ao Contrato/Serviço ID:' + str(andamento.pedido.pk) + ' disponível no App. Acesse e confira o andamento mais recente.'
-            else: #elif (self.kwargs['origem'] is 'solicitacao'):
+                message = 'Referente ao Contrato/Serviço "' + str(andamento.servico.nome) + '" | Status atual: ' + andamento.status.nome + ' | Informação adicionada: ' + andamento.comentario
+                html_message= 'Este e-mail refere-se ao Contrato/Serviço <b>"' + str(andamento.servico.nome) + '"</b><br>Status atual: <b>' + andamento.status.nome + '</b><br>Informação adicionada: <b>' + andamento.comentario + '</b>'
+                message_onesignal = 'Atualização referente ao Contrato/Serviço "' + str(andamento.servico.nome) + '" disponível no App. Acesse e confira o andamento mais recente.'
+            else: #elif (self.kwargs['origem'] == 'solicitacao'):
                 emailContato = andamento.solicitacao.cliente.emailContato ## para quem vai a mensagem
                 player_id_onesignal = andamento.solicitacao.cliente.playerId_onesignal
-                message = 'Referente a Solicitação ID: ' + andamento.solicitacao.solicitacao + ' | Status atual: ' + andamento.status.nome + ' | Informação adicionada: ' + andamento.comentario
-                html_message= 'Este e-mail refere-se a Solicitação <b>ID: ' + andamento.solicitacao.solicitacao + '</b><br>Status atual: <b>' + andamento.status.nome + '</b><br>Informação adicionada: <b>' + andamento.comentario + '</b>'
-                message_onesignal = 'Atualização referente a Solicitação ID: ' + andamento.solicitacao.solicitacao + ' disponível no App. Acesse e confira o andamento mais recente.'
+                message = 'Referente a Solicitação "' + andamento.solicitacao.solicitacao + '" | Status atual: ' + andamento.status.nome + ' | Informação adicionada: ' + andamento.comentario
+                html_message= 'Este e-mail refere-se a Solicitação <b>"' + andamento.solicitacao.solicitacao + '"<br>Status atual: <b>' + andamento.status.nome + '</b><br>Informação adicionada: <b>' + andamento.comentario + '</b>'
+                message_onesignal = 'Atualização referente a Solicitação "' + andamento.solicitacao.solicitacao + '" disponível no App. Acesse e confira o andamento mais recente.'
             email_from = self.request.user.empregado.email #settings.EMAIL_HOST_USER
             recipient_list = [emailContato, email_from,]
             ## EXEMPLO COMPLETO: send_mail(subject, message, from_email, recipient_list, fail_silently=False, auth_user=None, auth_password=None, connection=None, html_message=None)
@@ -107,7 +118,9 @@ class AndamentoNovo(CreateView):
 
         if (str(self.kwargs['origem']) == 'pedido'):
             return redirect('list_pedidos')
-        else: #elif (self.kwargs['origem'] is 'solicitacao'):
+        elif (str(self.kwargs['origem']) == 'cliente'):
+            return redirect('list_clientes')
+        else: #elif (self.kwargs['origem'] == 'solicitacao'):
             return redirect('list_solicitacoes')
 
 
